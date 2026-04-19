05d203e1b46d573c3d54c6cdbec0052b4bb660b9
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-04-18 23:35:40 -0300

    remove unused configuration files and clean up dwm source code

--- config.def.h
+++ config.def.h
@@ -1,24 +1,20 @@
-/* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx  = 1;
+static const unsigned int snap      = 32;
+static const int showbar            = 1;
+static const int topbar             = 1;
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
 
-/* Dracula Theme */
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#ffb86c";
 static const char col_gray3[]       = "#ff79c6";
-static const char col_gray4[]       = "#ffb86c";
 static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
-    /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
+    [SchemeSel]  = { col_gray2, col_cyan,  col_gray2  },
 };
 
 static const char *tags[] = { "1", "2", "3", "4", "5" };
@@ -43,9 +39,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile }
 };
 
 /* key definitions */
@@ -55,50 +49,25 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
-
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0";
-static const char *dmenucmd[] = {
-	"j4-dmenu-desktop",
-	"--dmenu=dmenu -i -fn 'JetBrainsMono Nerd Font:size=11' -nb '#000000' -nf '#ff79c6' -sb '#ffb86c' -sf '#07080a' -p Run:",
-	NULL
-};
-static const char *termcmd[] = { "st", "-e", "/usr/bin/fish", NULL };
-static const char *files[]   = { "st", "-e", "yazi", NULL };
-static const char *browser[] = { "chromium", "--enable-features=VaapiVideoDecodeLinuxGL", NULL };
-static const char *flameshot[] = { "flameshot", "gui", NULL };
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
-static const char *brupcmd[] = { "brightnessctl", "set", "10%+", NULL };
-static const char *brdowncmd[] = { "brightnessctl", "set", "10%-", NULL };
-
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_v,      spawn,          SHCMD("/home/pedro/scripts/cliphist.sh sel") },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd} },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("passmenu") },
-	{ MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("/home/pedro/scripts/bookmarks.sh") },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshot} },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      spawn,          {.v = browser} },
-	{ MODKEY,                       XK_e,      spawn,          {.v = files} },
+	{ MODKEY,                       XK_p,      spawn,          SHCMD("j4-dmenu-desktop --dmenu=dmenu -i -fn 'JetBrainsMono Nerd Font:size=11' -nb '#000000' -nf '#ff79c6' -sb '#ffb86c' -sf '#07080a' -p Run:") },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("/home/pedrohenrique/.local/bin/rbw_menu.sh") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("/home/pedrohenrique/.local/bin/screenshot.sh") },
+	{ MODKEY,                       XK_Return, spawn,          SHCMD("st -e fish") },
+	{ MODKEY,                       XK_b,      spawn,          SHCMD("librewolf --new-instance") },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("st -e yazi") },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
 	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
@@ -121,38 +90,23 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn,          {.v = upvol } },
-        { 0,                            XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
-        { 0,                            XF86XK_AudioMute,        spawn,          {.v = mutevol } },
-	{ 0,                            XF86XK_MonBrightnessUp,   spawn, {.v = brupcmd } },
-        { 0, XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,               {.i = +1 } },
+	TAGKEYS(                        XK_1,                            0)
+	TAGKEYS(                        XK_2,                            1)
+	TAGKEYS(                        XK_3,                            2)
+	TAGKEYS(                        XK_4,                            3)
+	TAGKEYS(                        XK_5,                            4)
+	{ MODKEY|ShiftMask,             XK_q,                     quit,  {0} },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn, SHCMD("amixer sset PCM 5%+") },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn, SHCMD("amixer sset PCM 5%-") },
+	{ 0,                            XF86XK_AudioMute,         spawn, SHCMD("amixer sset PCM toggle") },
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn, SHCMD("brightnessctl set 10%+") },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn, SHCMD("brightnessctl set 10%-") },
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
